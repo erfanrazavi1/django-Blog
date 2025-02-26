f1b3998d5d9357e847b6b93af39186461792efa7
Erfan Razavi <101706905+erfanrazavi@users.noreply.github.com>
2025-02-26 23:33:14 +0330

    🔐 Add custom user authentication with email login
    Added EmailAuthBackend for authentication using email instead of username
    Created RegisterForm and CustomLoginForm for user authentication
    Implemented views for user registration (register_view), login (login_view), and logout (logout_view)
    Configured AUTHENTICATION_BACKENDS to use email-based authentication
    Added URL patterns for authentication (register, login, logout)

---
+++ core/accounts/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from accounts.models import CustomUser
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate
+
+
+class RegisterForm(forms.ModelForm):
+    phone = forms.CharField(label="تلفن همراه", required=False)
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_confirm = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'password', 'password_confirm']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password_confirm = cleaned_data.get('password_confirm')
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError("رمز عبور و تایید آن یکسان نیستند")
+        return cleaned_data
+
+class CustomLoginForm(forms.Form):
+    email = forms.CharField(label="ایمیل")
+    password = forms.CharField(widget=forms.PasswordInput, label="رمز عبور")
+    def clean(self):
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        if email and password:
+            user = authenticate(username=email, password=password)  # تغییر username به email
+            if not user:
+                raise forms.ValidationError("ایمیل یا رمز عبور نادرست است.")
+            self.user = user
+        return self.cleaned_data
+
+    def get_user(self):
+        return self.user
--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,1 +1,45 @@
 
+from django.shortcuts import render, redirect
+from django.urls import  reverse
+from django.contrib.auth import login, authenticate, logout
+from accounts.forms import RegisterForm, CustomLoginForm
+from accounts.models import CustomUser
+from django.contrib import messages
+
+# ثبت‌نام کاربر
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data["password"])
+            user.save()
+            messages.success(request, "ثبت‌نام با موفقیت انجام شد! حالا وارد شوید.")
+            return redirect(reverse('accounts:login'))
+    else:
+        form = RegisterForm()
+    return render(request, "registration/register.html", {"form": form})
+
+
+# ورود کاربر
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect(reverse('blog:index'))
+
+    if request.method == "POST":
+        form = CustomLoginForm(request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, "ورود موفقیت‌آمیز بود!")
+            return redirect(reverse('blog:index'))
+    else:
+        form = CustomLoginForm()
+    return render(request, "registration/login.html", {"form": form})
+
+
+# خروج کاربر
+def logout_view(request):
+    logout(request)
+    messages.success(request, "شما با موفقیت خارج شدید!")
+    return redirect(reverse('blog:index'))
