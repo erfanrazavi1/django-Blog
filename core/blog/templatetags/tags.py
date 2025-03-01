89cc30e770b4359cfa5d50ee5522f96f8aeae502
Erfan Razavi <101706905+erfanrazavi@users.noreply.github.com>
2025-03-01 15:42:51 +0330

    Fix redundant return and add get_all_posts tag
     to fetch all posts.
    Minor improvements for readability.

--- core/blog/templatetags/tags.py
+++ core/blog/templatetags/tags.py
@@ -1,5 +1,7 @@
 import jdatetime
 from django import template
+from blog.models import Post
+from django.shortcuts import render
 
 register = template.Library()
 
@@ -8,3 +10,7 @@ def to_jalali(value):
     if value:
         return jdatetime.datetime.fromgregorian(datetime=value).strftime("%Y/%m/%d")
     return ""
+
+@register.simple_tag
+def get_all_posts():
+    return Post.objects.all()
