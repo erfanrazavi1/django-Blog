8aa7d346600eec33d5f86c9ea5b90b5de710415d
Erfan Razavi <101706905+erfanrazavi@users.noreply.github.com>
2025-03-02 06:22:52 +0330

    Added pagination to the blog post list
    Implemented pagination for the blog post list using paginate_by = 3.
    Updated the template to include a pagination navigation bar.
    Users can navigate between pages using "Previous" and "Next" buttons.
    The current page is highlighted in the pagination bar.

--- core/blog/views.py
+++ core/blog/views.py
@@ -19,7 +19,7 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
-    
+    paginate_by = 3
     def get_queryset(self):
         return Post.objects.filter(status=True).order_by('-created_date')
 
