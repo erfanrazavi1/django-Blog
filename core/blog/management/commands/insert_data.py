4c372c885f4c04d780e8a68ad590547783bbea1c
Erfan Razavi <erfan6235@gmail.com>
2025-04-22 20:11:38 +0330

    Update insert_data.py

--- core/blog/management/commands/insert_data.py
+++ core/blog/management/commands/insert_data.py
@@ -10,14 +10,15 @@ category_list = [
     "Food",
 ]
 
+
 class Command(BaseCommand):
     help = "Generate fake data for CustomUser model"
-    
-    def __init__(self,  stdout=None, stderr=None, no_color=False, force_color=False):
+
+    def __init__(self, stdout=None, stderr=None, no_color=False, force_color=False):
         """Initialize the command with custom stdout and stderr."""
         super(Command, self).__init__(stdout, stderr, no_color, force_color)
         self.fake = Faker()
-    
+
     def handle(self, *args, **kwargs):
         user = CustomUser.objects.create_user(
             email=self.fake.email(),
@@ -31,16 +32,20 @@ class Command(BaseCommand):
         profile.last_name = self.fake.last_name()
         profile.description = self.fake.text()
         profile.save()
-        
+
         for name in category_list:
-            Category.objects.get_or_create(name=name) # get_or_create will not create duplicate categories
-        
+            Category.objects.get_or_create(
+                name=name
+            )  # get_or_create will not create duplicate categories
+
         for _ in range(10):
             post = Post.objects.create(
                 author=profile,
                 title=self.fake.sentence(),
                 content=self.fake.text(),
                 status=True,
-                category=Category.objects.get(name=self.fake.random_element(elements=category_list)),
+                category=Category.objects.get(
+                    name=self.fake.random_element(elements=category_list)
+                ),
             )
             self.stdout.write(self.style.SUCCESS(f"Created Post: {post.title}"))
